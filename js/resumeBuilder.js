68e91f928f8395ae60fe7f3b62e4df822da98045
Shubhansh Vatsyayan <vshubhansh@gmail.com>
2017-07-27 22:32:09 +0530

    First commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var awesomeThoughts = "I am Shubhansh and I am awesome";
+var firstName = "Shubhansh";
+var age = 32;
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("awesome","fun");
+console.log(funThoughts);
+var name = "Shubhansh Vatsyayan";
+var formattedName = HTMLheaderName.replace("%data%",name);
+$("#main").append("Shubhansh Vatsyayan");
+$("#main").append(funThoughts);
+$("#header").append(formattedName);
